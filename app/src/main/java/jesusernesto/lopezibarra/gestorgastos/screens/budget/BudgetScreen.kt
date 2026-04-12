7e641e0bdb8da8622e6cfe1210d5a23fa35eef36
Chavirez <santiagosanchezch@gmail.com>
2026-04-12 13:58:19 -0700

    Navigation

--- app/src/main/java/jesusernesto/lopezibarra/gestorgastos/screens/budget/BudgetScreen.kt
+++ app/src/main/java/jesusernesto/lopezibarra/gestorgastos/screens/budget/BudgetScreen.kt
@@ -1,6 +1,5 @@
 package jesusernesto.lopezibarra.gestorgastos.screens.budget
 
-import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -34,14 +33,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import jesusernesto.lopezibarra.gestorgastos.dummy.DummyData
-import jesusernesto.lopezibarra.gestorgastos.screens.graphs.AppTopBar
+import jesusernesto.lopezibarra.gestorgastos.screens.components.AppTopBar
 import jesusernesto.lopezibarra.gestorgastos.ui.theme.DarkNavy
 import jesusernesto.lopezibarra.gestorgastos.ui.theme.GreenIncome
 import jesusernesto.lopezibarra.gestorgastos.ui.theme.Purple
